f14f0357bd8d668d91e534f5a1df34d1173f80f3
TOSS <tiago.medice@gft.com>
2020-10-17 20:15:03 +0200

    release

--- src/main/java/com/medici/Application.java
+++ src/main/java/com/medici/Application.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
  * @author JAMES MEDICI
- *
  */
 @SpringBootApplication
 public class Application {
