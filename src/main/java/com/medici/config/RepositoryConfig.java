32f0593c41b678748e8b6126907cd764ccdf47c8
TOSS <tiago.medice@gft.com>
2020-10-19 11:33:02 +0200

    jasypt config

--- src/main/java/com/medici/config/RepositoryConfig.java
+++ src/main/java/com/medici/config/RepositoryConfig.java
@@ -1,9 +1,25 @@
 package com.medici.config;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 /**
  * 
@@ -11,9 +27,69 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  */
 @Configuration
-@ComponentScan("com.medici")
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan("com.medici.**")
 @EntityScan("com.medici.model")
 @EnableJpaRepositories("com.medici.repository")
+@EnableEncryptableProperties
 public class RepositoryConfig {
+
+	private static final String JASYPT = "jasypt";
+
+	private static final String PBE_WITH_HMACSHA512_AND_AES_256 = "PBEWithHMACSHA512AndAES_256";
+
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	@Value("${spring.datasource.driver-class-name}")
+	private String driver;
+
+	@Value("${spring.datasource.username}")
+	private String username;
+
+	@Value("${spring.datasource.password}")
+	private String password;
+
+	@Bean
+	public DataSource dataSource() {
+
+		String passwordDecoded = encryptor().decrypt(password);
+
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName(driver);
+		dataSourceBuilder.url(url);
+		dataSourceBuilder.username(username);
+		dataSourceBuilder.password(passwordDecoded);
+		return dataSourceBuilder.build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setPackagesToScan("com.medici");
+		factory.setDataSource(dataSource());
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(emf);
+		return txManager;
+	}
+
+	@Bean
+	public StandardPBEStringEncryptor encryptor() {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setPassword(JASYPT);
+		encryptor.setAlgorithm(PBE_WITH_HMACSHA512_AND_AES_256);
+		encryptor.setIvGenerator(new RandomIvGenerator());
+		return encryptor;
+
+	}
 
 }
